0b07b170ad4f60b8cb7b37375b63bbde51fda1d5
Jeremy Bivens <jerbbive@ut.utm.edu>
2019-10-10 13:38:14 -0500

    Add project files.

---
+++ ClassWork9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+
+            Console.WriteLine("Push any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
